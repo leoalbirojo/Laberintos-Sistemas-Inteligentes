483645e2080067c8953c0f9894ce2317dd69fbba
leoalbirojo <leoalbirojo@git.com>
2015-03-07 18:56:19 -0500

    Carga de archivos

---
+++ README.md
@@ -0,0 +1,7 @@
+Automatas recorredores de laberintos
+
+
+Requerimientos
+JDK 1.8
+
+Sólo archivo fuente, no es un proyecto de Netbeans o Eclipse
---
+++ src/unalcol/agents/examples/labyrinth/teseo/simple/LeoAgent.java
@@ -0,0 +1,13 @@
+package unalcol.agents.examples.labyrinth.teseo.simple;
+
+public class LeoAgent extends SimpleTeseoAgentProgram{
+	
+	
+
+	@Override
+	public int accion(boolean PF, boolean PD, boolean PA, boolean PI, boolean MT) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
